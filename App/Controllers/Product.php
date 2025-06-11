491d0e13cecbfec6e852fa44455c8ba2ab6d2b3c
Matht64 <mattmattvias@yahoo.fr>
2025-06-11 21:15:30 +0200

    formulaire de contact fonctionnel

--- App/Controllers/Product.php
+++ App/Controllers/Product.php
@@ -5,6 +5,7 @@ namespace App\Controllers;
 use App\Models\Articles;
 use App\Utility\Upload;
 use \Core\View;
+use TypeError;
 
 /**
  * Product controller
@@ -19,12 +20,12 @@ class Product extends \Core\Controller
     public function indexAction()
     {
 
-        if(isset($_POST['submit'])) {
+        if (isset($_POST['submit'])) {
 
             try {
                 $f = $_POST;
                 // Done: Validation
-                if ($f['title'] == "" or $f['description'] == "" or $f['city'] == "" or $f['picture'] == ""){
+                if ($f['title'] == "" or $f['description'] == "" or $f['city'] == "" or $f['picture'] == "") {
                     throw new \Exception("Empty Fields, fill all the required fields");
                 }
 
@@ -36,8 +37,8 @@ class Product extends \Core\Controller
                 Articles::attachPicture($id, $pictureName);
 
                 header('Location: /product/' . $id);
-            } catch (\Exception $e){
-                    var_dump($e);
+            } catch (\Exception $e) {
+                var_dump($e);
             }
         }
 
@@ -56,7 +57,7 @@ class Product extends \Core\Controller
             Articles::addOneView($id);
             $suggestions = Articles::getSuggest();
             $article = Articles::getOne($id);
-        } catch(\Exception $e){
+        } catch (\Exception $e) {
             var_dump($e);
         }
 
@@ -65,4 +66,41 @@ class Product extends \Core\Controller
             'suggestions' => $suggestions
         ]);
     }
+
+    /**
+     * Affiche le formulaire de contact pour un produit
+     * @return void
+     */
+    public function contactAction()
+    {   
+        $product_id = $this->route_params['id'];
+        try {
+            Articles::addOneView($product_id);
+            $article = Articles::getOne($product_id);
+        } catch (\Exception $e) {
+            var_dump($e);
+        }
+        
+        if (isset($_POST['submit'])) {
+
+            try {
+                $f = $_POST;
+                // Done: Validation
+                if ($f['message'] == "") {
+                    throw new \Exception("Empty Fields, fill all the required fields");
+                }
+                
+                header('Location: /product/' . $product_id);
+            } catch (\Exception $e) {
+                var_dump($e);
+            }
+        }
+
+        View::renderTemplate('Product/Contact.html', [
+            'contacted_username' => $article[0]['username'],
+            'article' => $article[0]['name'],
+        ]);
+    }
+
+
 }
