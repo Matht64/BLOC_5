91dc94507cb12827b972fc6b2b37cdd666dee2ed
EwennV <ewenn.vallois.perso@gmail.com>
2025-06-25 01:02:36 +0200

    Suppression du revert #19

--- App/Controllers/User.php
+++ App/Controllers/User.php
@@ -40,14 +40,12 @@ class User extends \Core\Controller
     private function login($data)
     {
         try {
-            echo "Tentative de login...<br>";
 
             if (!isset($data['email']) || !isset($data['password'])) {
                 echo "Champs manquants<br>";
                 return false;
             }
 
-            echo "Email reçu : " . $data['email'] . "<br>";
             $user = \App\Models\User::getByLogin($data['email']);
 
             if (!$user) {
@@ -55,12 +53,7 @@ class User extends \Core\Controller
                 return false;
             }
 
-            echo "Utilisateur trouvé :<pre>";
-            print_r($user);
-            echo "</pre>";
-
             $hashed = Hash::generate($data['password'], $user['salt']);
-            echo "Hash généré : $hashed<br>";
 
             if ($hashed !== $user['password']) {
                 echo "Mot de passe incorrect<br>";
@@ -73,8 +66,6 @@ class User extends \Core\Controller
                 'is_admin' => $user['is_admin'],
             ];
 
-            echo "<pre>SESSION : ";
-            print_r($_SESSION);
             echo "</pre>";
             return true;
 
