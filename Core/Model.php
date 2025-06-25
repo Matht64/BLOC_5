a3789e7d33cd86be0174369de63e58960bb96ec4
Matht64 <mattmattvias@yahoo.fr>
2025-06-25 09:18:49 +0200

    add responsive + fix

--- Core/Model.php
+++ Core/Model.php
@@ -22,12 +22,19 @@ abstract class Model
         static $db = null;
         
         if ($db === null) {
-            $dsn = 'mysql:host=' . getenv('DATABASE_HOST') . ';dbname=' . getenv('DATABASE_NAME') . ';charset=utf8';
-            $db = new PDO($dsn, getenv('DATABASE_USER'), getenv('DATABASE_PASSWORD'));
+            $dsn = 'mysql:host=localhost;dbname=videgrenierenligne;charset=utf8';
+            $db = new PDO($dsn, "root", '');
 
             // Throw an Exception when an error occurs
             $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         }
+        // if ($db === null) {
+        //     $dsn = 'mysql:host=' . getenv('DATABASE_HOST') . ';dbname=' . getenv('DATABASE_NAME') . ';charset=utf8';
+        //     $db = new PDO($dsn, getenv('DATABASE_USER'), getenv('DATABASE_PASSWORD'));
+
+        //     // Throw an Exception when an error occurs
+        //     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        // }
 
         return $db;
     }
